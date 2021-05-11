ccc9dd12bb0b4af16ef58edf459529db4c484b14
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-05-11 11:12:10 +0200

    over

---
+++ dom_recuperation_elements_part1_exercice1/main.js
@@ -0,0 +1,12 @@
+let h1 = document.getElementsByTagName("h1")
+console.log(h1);
+
+let h31 = document.getElementsByTagName("h3")[0]
+console.log(h31);
+let h32 = document.getElementsByTagName("h3")[1]
+console.log(h32);
+let h33 = document.getElementsByTagName("h3")[2]
+console.log(h33);
+
+let li = document.getElementsByTagName("li")[2]
+console.log(li);
